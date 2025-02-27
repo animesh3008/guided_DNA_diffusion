d761c867f2b2df02c61ec6dcf87cf429de556d12
animesh3008 <animeshawasthi2000@gmail.com>
2025-02-27 16:58:49 +0100

    initial commit

--- drakes_protein/fmif/scripts/zeroalpha.sh
+++
@@ -1,5 +0,0 @@
-for seed in 0 1 2
-do
-    echo "Running Zero Alpha... with seed=$seed"
-    CUDA_VISIBLE_DEVICES=1 python eval_finetune.py --decoding=original --base_model=zero_alpha --seed=$seed
-done
